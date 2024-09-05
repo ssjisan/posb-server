458ea9ad0725519c3dec69379c9ef96db9f15f03
ssjisan <ssjisan.dev@gmail.com>
2024-09-05 19:01:31 +0600

    21 days set

--- middlewares/expireLinks.js
+++ middlewares/expireLinks.js
@@ -2,9 +2,13 @@ import cron from 'node-cron';
 import Events from '../model/eventModel.js'; // Adjust path as necessary
 
 // Schedule the job to run every 5 minutes
-cron.schedule('*/2 * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
   try {
     const now = new Date();
+    // Calculate the date 21 days ago from now
+    const expireDate = new Date(now);
+    expireDate.setDate(expireDate.getDate() - 21);
+
     // Find events where the registration link should expire
     await Events.updateMany(
       { 
